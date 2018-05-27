2a1dac9cae0b3d010d52d34f1042e1b8810e4333
dv <suchkov.dv@gmail.com>
2018-05-27 18:53:16 +0300

    v 1.0

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
-# Frequency Analysis of Words
+# Подсчет наиболее частро встречающихся слов в тексте
+Скрипт выводит 10 наиболее часто встречающихся в текстте слов
 
-[TODO. There will be project description]
 
-# Project Goals
+# Как использовать
+Запустить скрипт, указав через ключ `-f` файл, в котором будем считать слова
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+# Как запустить
+
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+
+Запуск на Linux:
+```bash
+python lang_frequency.py -f Garrison_-_Neukrotimaya_planeta.txt
+     и : 1298
+    не : 1005
+     в : 827
+    на : 791
+   что : 703
+  Язон : 593
+    он : 524
+     с : 503
+   его : 361
+    то : 338
+```
+
+Запуск на Windows происходит аналогично.
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке ― [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,70 @@
+import argparse
+import re
+from collections import Counter
+import chardet
 
 
 def load_data(filepath):
-    pass
+    return open(filepath, 'rb').read()
 
 
 def get_most_frequent_words(text):
-    pass
+    return Counter(text.split(' ')).most_common((10))
+
+
+def decode_text(text_bytes):
+    most_common_charsets = ['utf8', 'cp1251']
+    for charset in most_common_charsets:
+        try:
+            return text_bytes.decode(charset)
+        except ValueError:
+            pass
+
+    try:
+        result = chardet.detect(text_bytes)
+        return text_bytes.decode(result['encoding'])
+    except ValueError:
+        return
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-f', action='store',
+        dest='filepath',
+        help='Filepath with text, "book.txt" by default',
+        default='book.txt'
+    )
+
+    return parser.parse_args()
+
+
+def print_list_of_tuples(list_of_tuples):
+    for i_tuple in list_of_tuples:
+        print('{:>6} : {} '.format(i_tuple[0], i_tuple[1]))
 
 
 if __name__ == '__main__':
-    pass
+    params = parse_arguments()
+
+    try:
+        text_bytes = load_data(params.filepath)
+    except ValueError as e:
+        print('Не могу прочитать данные из файла {}'.format(params.filepath))
+        exit()
+    except OSError as e:
+        print('Файл {} не существует '.format(params.filepath))
+        exit()
+
+    text = decode_text(text_bytes)
+    if text is None:
+        raise ValueError('Не могу декодировать данные')
+
+    text = re.sub(r'([^\w\d]|_)+', ' ', text)
+    try:
+        most_common_word_list = (get_most_frequent_words(text))
+    except ValueError:
+        print('Не могу посчитать')
+
+    print_list_of_tuples(most_common_word_list)
